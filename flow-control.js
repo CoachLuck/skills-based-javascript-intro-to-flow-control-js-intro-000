9aed6aebabd223024de6d62f3b828c30cb4d7180
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 17:39:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,17 +9,16 @@ function teenager(age) {
     return "You are a teenager!"
   }
 
-  return "You are not a teenager.";
+  return "You are not a teenager";
 }
 
 function ageChecker(age) {
-  if (age < 12) {
-    return "You are a kid"
-  } else if (age > 12 && age < 20) {
-    return "You are a teenager!"
-  }
-  else {
+  if (age > 19) {
     return "You are a grownup"
+  } else if (age > 12) {
+    return "You are a teenager"
+  } else {
+    return "You are a kid"
   }
 }
 
